09aa435e16d641bd43ab85a06319e844c1d0fa49
William <w.chau@windesheim.nl>
2022-03-06 18:36:47 +0000

    commit voorbeeldbranch4

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
         <h1>Hello World!</h1>
         <h2>This was added in a Git branch</h2>
         <h3>add another branch  </h3>
+        <h4>added by voorbeeldbranch4</h4>
       </header>
     </div>
   );
